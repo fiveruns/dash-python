f12afc2047ad1a07e2886af03bd2ba7400e00c44
Bruce Williams <bruce@codefluency.com>
2008-11-25 21:08:55 -0600

    Start building out Recipe structure

--- fiveruns_dash/configuration.py
+++ fiveruns_dash/configuration.py
@@ -3,20 +3,11 @@ import os, sys
 import metrics
 from logging import log
 
-class Configuration(object):
-
-  def __init__(self, **options):
+class MetricSetting(object):
+  
+  def __init__(self):
     self.metrics = {}
-    self.app_token = None
-    # TODO: Extract these into a Session object
-    self.vm_version = sys.version
-    self.pid = os.getpid()
-    self.pwd = os.getcwd()
-    self.process_id = None
-    self.report_interval = 60
-    for k, v in options.iteritems():
-      self.__dict__[k] = v
-
+  
   def time(self, name, description, **options):
     "Add a time metric"
     self.metrics[name] = metrics.TimeMetric(name, description, **options)
@@ -32,7 +23,31 @@ class Configuration(object):
   def percentage(self, name, description, **options):
     "Add a percentage metric"
     self.metrics[name] = metrics.PercentageMetric(name, description, **options)
+  
+
+class Configuration(MetricSetting):
+
+  def __init__(self, **options):
+    super(Configuration, self).__init__()
+    self.app_token = None
+    # TODO: Extract these into a Session object
+    self.vm_version = sys.version
+    self.pid = os.getpid()
+    self.pwd = os.getcwd()
+    self.process_id = None
+    self.report_interval = 60
+    for k, v in options.iteritems():
+      self.__dict__[k] = v
 
   def instrument(self):
     for metric in self.metrics.values():
       metric._instrument()
+      
+class Recipe(MetricSetting):
+  """docstring for Recipe"""
+  
+  def __init__(self, name, url):
+    super(Recipe, self).__init__()
+    self.name = name
+    self.url = url
+    
