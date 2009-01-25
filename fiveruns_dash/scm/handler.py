ee80c2874571056b4a6aac1078b7d54e70e0e148
Bruce Williams <bruce@codefluency.com>
2009-01-25 02:56:05 -0600

    Support SVN

--- fiveruns_dash/scm/handler.py
+++ fiveruns_dash/scm/handler.py
@@ -1,30 +1,30 @@
 import os, logging
 
-logger = logging.getLogger('fiveruns_dash.scm.handlers.git')
+logger = logging.getLogger('fiveruns_dash.scm')
 
 class Handler(object):
 
-  def __init__(self, match):
-    self.data = {}
-    self.match = match
-    self.collected = self.collect()
+    def __init__(self, match):
+        self.data = {}
+        self.match = match
+        self.collected = self.collect()
 
-  def collect(self):
-    """
-    Collect the data for this SCM
-    
-    Needs to be defined in each handler or an exception is raised.
-    """
-    raise NotImplementedError("Abstract")
+    def collect(self):
+        """
+        Collect the data for this SCM
 
-  def missing(self, pkg):
-    logger.warn("Missing `%s' package.  Unable to report SCM details" % pkg)
+        Needs to be defined in each handler or an exception is raised.
+        """
+        raise NotImplementedError("Abstract")
 
-  def __getitem__(self, key):
-    return self.data[key]
+    def missing(self, pkg):
+        logger.warn("Missing `%s' package.  Unable to report SCM details" % pkg)
 
-  def keys(self):
-    return self.data.keys()
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def keys(self):
+        return self.data.keys()
 
 def handlers():
     """
@@ -34,25 +34,27 @@ def handlers():
             if not f.startswith('_') and f.endswith('.py')]
 
 def at(directory):
-  for name in handlers():
-    match = _grab(name, 'matches')(directory)
-    if match:
-      handler = _grab(name, 'Handler')(match)
-      if handler.collected:
-        return handler
+    for name in handlers():
+        match = _grab(name, 'matches')(directory)
+        if match:
+            logger.info("Found SCM: %s" % name)
+            handler = _grab(name, 'Handler')(match)
+            if handler.collected:
+                return handler
+    logger.warn("Could not find SCM")
 
 def _grab(name, attrib):
-  return getattr(__import__('fiveruns_dash.scm.handlers.%s' % name, {}, {}, [attrib]), attrib)
+    return getattr(__import__('fiveruns_dash.scm.handlers.%s' % name, {}, {}, [attrib]), attrib)
 
 def ascend(matcher, dirname, name):
-  """
-  Walk up a directory structure looking for a matching location
-  """
-  if matcher(dirname, name):
-    return dirname
-  else:
-    if dirname in ('/', ''):
-      return False
+    """
+    Walk up a directory structure looking for a matching location
+    """
+    if matcher(dirname, name):
+        return dirname
     else:
-      return ascend(matcher, *os.path.split(dirname))
-  
+        if dirname in ('/', ''):
+            return False
+        else:
+            return ascend(matcher, *os.path.split(dirname))
+
---
+++ fiveruns_dash/scm/handlers/svn.py
@@ -0,0 +1,30 @@
+import os, logging, time
+from fiveruns_dash.scm import handler
+
+logger = logging.getLogger('fiveruns_dash.scm.handlers.svn')
+
+class Handler(handler.Handler):
+
+    def collect(self):
+        self._check_info()
+        if not len(self.data):
+            return False
+        self.data['scm_type'] = 'svn'
+        return True
+
+    def _check_info(self):
+        try:
+            fn =  os.popen("svn info '%s'" % self.match)
+        except:
+            logger.warn("Could not access `svn info,` is `svn` in PATH?")
+        mapping = {'URL': 'scm_url', 'Revision': 'scm_revision'}
+        for line in fn.readlines():
+            pair = [x.strip() for x in line.split(':', 1)]
+            if mapping.has_key(pair[0]):
+                self.data[mapping[pair[0]]] = pair[1]
+
+def matches(directory):
+    matcher = lambda dir, fn: os.path.isdir(os.path.join(dir, '.svn'))
+    found = handler.ascend(matcher, directory, '')
+    if found:
+        return os.path.join(found, '.svn')
