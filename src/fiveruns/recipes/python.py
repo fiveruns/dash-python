d97a7ed7f46e3ee9e84682c38a66e4e7d3ac3497
Aaron Farnham <afarnham@gmail.com>
2009-05-17 21:18:08 -0500

    Fixed cpu recipe

--- src/fiveruns/recipes/python.py
+++ src/fiveruns/recipes/python.py
@@ -32,13 +32,17 @@ cpu_before = None
 def _cpu():
     global cpu_before
     if not cpu_before:
-        cpu_before = os.times()[0]
+        cpu_before = os.times()
 
-    cpu_after = os.times()[0]
-    this_minute = cpu_after - cpu_before
+    cpu_after = os.times()
+    
+    user_time = cpu_after[0] - cpu_before[0]
+    system_time = cpu_after[1] - cpu_before[1]
+    #this_minute = cpu_after - cpu_before
+    cpu_time = user_time + system_time
     cpu_before = cpu_after
 
-    return (this_minute / 60.0) * 100.00
+    return (system_time / 60.0) * 100.00
 
 def _refcount():
     ''' Get number of system refcounts total. '''
@@ -49,6 +53,6 @@ def _refcount():
 python_recipe.absolute('vsz', 'Virtual Memory Usage', unit = 'kbytes', call = _vsz)
 python_recipe.absolute('rss', 'Resident Memory Usage', unit = 'kbytes', call = _rss)
 python_recipe.percentage('pmem', 'Percentage Resident Memory Usage', call = _pmem)
-#python_recipe.percentage('cpu', 'Percentage CPU Usage', call = _cpu)
+python_recipe.percentage('cpu', 'Percentage CPU Usage', call = _cpu)
 python_recipe.absolute("gc_objects", "Number of GC tracked objects", call=_refcount)
 
